dc3d52f950db77f6b55c6e12558af6fe4d656e4e
Jörgen Bengtsson <jorgen.bengtsson@guardtools.com>
2024-05-22 10:04:21 +0200

    Add Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import { useEffect, useState } from 'react';
 import CreateMirageServer from './mirage/create-mirage-server';
-
-import getData from './services/api';
 
 import './App.module.css';
 
@@ -10,23 +7,16 @@ if (process.env.REACT_APP_MODE === 'development') {
 }
 
 function App () {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    getData().then(data => {
-      setData(data.data);
-    });
-  }, []);
-
   return (
-    <div>
-      <div>Application</div>
-      <div>
-        <div>Mode: {process.env.REACT_APP_MODE}</div>
-      </div>
-      <div>
-        <div>Data: {data.map(data => <div key={data.id}>{data.data}</div>)}</div>
-      </div>
+    <div className="h-screen flex items-center justify-center">
+        <div className="flex flex-row items-center justify-center m-10">
+          <div>
+            <div>Col 1</div>
+          </div>
+          <div>
+            <div>Col 2</div>
+          </div>
+        </div>
     </div>
   );
 }
